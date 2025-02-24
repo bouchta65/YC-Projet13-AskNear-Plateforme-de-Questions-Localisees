a933a401655234bdd929a45cb8271219ea15ce81
bouchta65 <bouchtamohamed01@gmail.com>
2025-02-24 10:39:57 +0100

    Enhance QuestionController with popular questions and user-specific questions functionality

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use App\Models\Favori;
 use App\Models\Reponse;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+
 
 class QuestionController extends Controller
 {
@@ -25,39 +29,24 @@ class QuestionController extends Controller
         $question->content = $validated['content'];
         $question->location = $validated['location'];
         $question->published_at = now();
-        $question->user_id = 1;
+        $userId = Auth::user()->id;
+        $question->user_id = $userId;
         $question->save();
-        return redirect('/');
+        return back();
 
     }
 
-    public function edit(Request $request , $id){
-        $question = Question::find($id);
-
-        $validated = $request->validate([
-            'title' => 'required|max:255',
-            'content' => 'required',
-            'location' => 'required',
-        ]);
-        
-        $question = new Question();
-        $question->title = $validated['title'];
-        $question->content = $validated['content'];
-        $question->location = $validated['location'];
-        $question->published_at = now();
-        $question->user_id = 1;
-        $question->save();
-        return "Question enregistrée";
-    }
-
-    public function delete(Request $request , $id){
+    public function destroy($id){
         $question = Question::find($id);
         $question->delete();
-        return "Question enregistrée";
+        return back();
     }
 
     public function show(){
         $questions = Question::WithCount('Reponses')->orderBy('created_at', 'desc')->get();
+        foreach ($questions as $question) {
+            $question->published_at = Carbon::parse($question->published_at)->format('F j, Y');  
+        }
         $countQuestions = $questions->count();
         $countReponses = Reponse::count();
         $countUsers = User::count();
@@ -70,11 +59,50 @@ class QuestionController extends Controller
         if (!$question) {
             abort(404); 
         }
+        $question = Question::where('id', $id)->with('user')->first();
+        $question->published_at = Carbon::parse($question->published_at)->format('F j, Y');
+
         $reponses = Reponse::where('question_id', $id)->with('user')->get();
+        foreach ($reponses as $reponse) {
+            $reponse->published_at = Carbon::parse($reponse->published_at)->format('F j, Y');
+        }
+        
         $countReponses = $reponses->count(); 
 
         return view("questions.details",compact('reponses','question','countReponses'));
     }
+
+    public function populaireQuestion(){
+        $questions = Question::WithCount('Reponses')->orderBy('created_at', 'desc')->get();
+        foreach ($questions as $question) {
+            $question->published_at = Carbon::parse($question->published_at)->format('F j, Y');  
+        }
+        $countQuestions = $questions->count();
+        $countReponses = Reponse::count();
+        $countUsers = User::count();
+        $popularQuestions = Question::withCount('Reponses')->orderByDesc('reponses_count')->take(3)->get();
+            return view("questions.populaires",compact('questions','countQuestions','countReponses','countUsers','popularQuestions'));
+    }
+
+
+    public function showByUser(){
+        if (Auth::check()) {
+            $userId = Auth::user()->id;
+            $questions = Question::where('user_id', $userId)->WithCount('Reponses','Favoris')->orderBy('created_at', 'desc')->get();
+            foreach ($questions as $question) {
+                $question->published_at = Carbon::parse($question->published_at)->format('F j, Y');
+            }
+            $countQuestions = $questions->count();
+            $Reponses = Reponse::where('user_id', $userId);
+            $countReponses = $Reponses->count();
+            return view("profille",compact('questions','countQuestions','countReponses'));
+        } else {
+            return redirect()->route('login');
+        }
+
+    }
+
+
     
 
  
